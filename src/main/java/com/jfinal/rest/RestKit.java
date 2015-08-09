a3fbd528fee67f7222164429af1ce65728f6facd
peak <taijunfeng_it@sina.com>
2015-08-09 23:57:58 +0800

    修正之前截取斜杠的逻辑问题

--- src/main/java/com/jfinal/rest/RestKit.java
+++ src/main/java/com/jfinal/rest/RestKit.java
@@ -1,17 +1,17 @@
 /**
- *  Copyright 2015 Peak Tai,台俊峰(taijunfeng_it@sina.com)
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Copyright 2015 Peak Tai,台俊峰(taijunfeng_it@sina.com)
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.jfinal.rest;
 
@@ -68,5 +68,20 @@ public class RestKit {
         }
     }
 
+    /**
+     * 切割key里包含的斜杠，去掉末尾的，保留开头的（没有就加上）
+     *
+     * @param key
+     * @return
+     */
+    static String cutSlash(String key) {
+        if (!key.startsWith("/")) {
+            key = "/" + key;
+        }
+        if (key.endsWith("/")) {
+            key = key.substring(0, key.length() - 1);
+        }
+        return key;
+    }
 
 }
--- src/main/java/com/jfinal/rest/RestRoutes.java
+++ src/main/java/com/jfinal/rest/RestRoutes.java
@@ -1,17 +1,17 @@
 /**
- *  Copyright 2015 Peak Tai,台俊峰(taijunfeng_it@sina.com)
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Copyright 2015 Peak Tai,台俊峰(taijunfeng_it@sina.com)
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.jfinal.rest;
 
@@ -33,13 +33,7 @@ class RestRoutes {
     private Set<RestKey> restKeySet = new HashSet<RestKey>();
 
     RestRoutes(String visitPath, Routes routes) {
-        if (!visitPath.startsWith("/")) {
-            visitPath = "/" + visitPath;
-        }
-        if (visitPath.endsWith("/")) {
-            visitPath = visitPath.substring(0, visitPath.length() - 2);
-        }
-        this.visitPath = visitPath;
+        this.visitPath = RestKit.cutSlash(visitPath);
         this.routes = routes;
     }
 
@@ -48,14 +42,14 @@ class RestRoutes {
     }
 
     void addRoute(String restKey, Class<? extends Controller> controllerClass) {
-        if (!restKey.startsWith("/")) {
-            restKey = "/" + restKey;
-        }
-        routes.add(visitPath + restKey, controllerClass);
+        restKey = RestKit.cutSlash(restKey);
+
         RestKey key = new RestKey(restKey);
         if (restKeySet.contains(key)) {
             throw new RuntimeException("restKey重复：" + key);
         }
+
+        routes.add(visitPath + restKey, controllerClass);
         restKeySet.add(key);
     }
 
