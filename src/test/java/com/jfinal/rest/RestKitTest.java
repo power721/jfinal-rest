c99b66ca9d1691ff3ee93ab0dc1fa6e152fc6da6
power <power0721@gmail.com>
2015-08-11 19:47:02 +0800

    add RestKitTest.java

---
+++ src/test/java/com/jfinal/rest/RestKitTest.java
@@ -0,0 +1,59 @@
+package com.jfinal.rest;
+
+import com.jfinal.config.Handlers;
+import com.jfinal.config.Routes;
+import com.jfinal.core.Controller;
+import com.jfinal.handler.Handler;
+import org.easymock.EasyMock;
+import org.junit.Before;
+import org.junit.Test;
+import org.powermock.api.easymock.PowerMock;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class RestKitTest {
+
+    HttpServletRequest request;
+    Routes routes;
+
+    @Before
+    public void setUp() {
+        routes = new Routes() {
+            @Override
+            public void config() {
+                // empty
+            }
+        };
+
+        request = PowerMock.createNicePartialMock(HttpServletRequest.class, "getMethod");
+        EasyMock.expect(request.getMethod()).andReturn("GET").anyTimes();
+    }
+
+    @Test
+    public void testBuildRoutes() throws Exception {
+        RestKit.buildRoutes("v1", routes, "com.jfinal.rest");
+
+        assertEquals(1, routes.getEntrySet().size());
+        Map.Entry<String, Class<? extends Controller>> entry = new ArrayList<Map.Entry<String, Class<? extends Controller>>>(routes.getEntrySet()).get(0);
+        assertEquals("/v1/tickets/:ticketId/messages", entry.getKey());
+        assertEquals(MockController.class, entry.getValue());
+    }
+
+    @Test
+    public void testBuildHandler() throws Exception {
+        Handlers handlers = new Handlers();
+        RestKit.buildRoutes("v1", routes, "com.jfinal.rest");
+
+        RestKit.buildHandler(handlers);
+
+        assertEquals(1, handlers.getHandlerList().size());
+        assertTrue(handlers.getHandlerList().get(0) instanceof RestHandler);
+    }
+
+}
